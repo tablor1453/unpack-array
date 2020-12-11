0ddc716ecd0f4b7d2e9f6768146b0369e27f8582
tablor1453 <rijalahmadt@gmail.com>
2020-12-11 15:32:25 +0700

    Rename file unpack.php to index

--- index.php
+++ index.php
@@ -1,113 +1,96 @@
 <?php
 
 
-$dataRaw = [
-    ['tahun_neraca' => "2015", "jumlah_bijih_SD" => 1000000, 'jumlah_bijih_CAD' => 20000000, "jumlah_logam_SD" => 1000000, 'jumlah_logam_CAD' => 20000000],
-    ['tahun_neraca' => "2016", "jumlah_bijih_SD" => 1000000, 'jumlah_bijih_CAD' => 20000000, "jumlah_logam_SD" => 1000000, 'jumlah_logam_CAD' => 20000000],
-    ['tahun_neraca' => "2016", "jumlah_bijih_SD" => 1000000, 'jumlah_bijih_CAD' => 20000000, "jumlah_logam_SD" => 1000000, 'jumlah_logam_CAD' => 20000000],
-    ['tahun_neraca' => "2016", "jumlah_bijih_SD" => 1000000, 'jumlah_bijih_CAD' => 20000000, "jumlah_logam_SD" => 1000000, 'jumlah_logam_CAD' => 20000000],
-    ['tahun_neraca' => "2017", "jumlah_bijih_SD" => 1000000, 'jumlah_bijih_CAD' => 20000000, "jumlah_logam_SD" => 1000000, 'jumlah_logam_CAD' => 20000000],
-    ['tahun_neraca' => "2017", "jumlah_bijih_SD" => 1000000, 'jumlah_bijih_CAD' => 20000000, "jumlah_logam_SD" => 1000000, 'jumlah_logam_CAD' => 20000000],
-    ['tahun_neraca' => "2018", "jumlah_bijih_SD" => 1000000, 'jumlah_bijih_CAD' => 20000000, "jumlah_logam_SD" => 1000000, 'jumlah_logam_CAD' => 20000000],
-    ['tahun_neraca' => "2018", "jumlah_bijih_SD" => 1000000, 'jumlah_bijih_CAD' => 20000000, "jumlah_logam_SD" => 1000000, 'jumlah_logam_CAD' => 20000000],
-    ['tahun_neraca' => "2018", "jumlah_bijih_SD" => 1000000, 'jumlah_bijih_CAD' => 20000000, "jumlah_logam_SD" => 1000000, 'jumlah_logam_CAD' => 20000000],
-];
+    $data = [
+        ['tahun_neraca' => "2015", "jumlah_bijih_SD" => 1000000, 'jumlah_bijih_CAD' => 20000000, "jumlah_logam_SD" => 1000000, 'jumlah_logam_CAD' => 20000000],
+        ['tahun_neraca' => "2015", "jumlah_bijih_SD" => 1000000, 'jumlah_bijih_CAD' => 20000000, "jumlah_logam_SD" => 1000000, 'jumlah_logam_CAD' => 20000000],
+        ['tahun_neraca' => "2016", "jumlah_bijih_SD" => 1000000, 'jumlah_bijih_CAD' => 20000000, "jumlah_logam_SD" => 1000000, 'jumlah_logam_CAD' => 20000000],
+        ['tahun_neraca' => "2016", "jumlah_bijih_SD" => 2000000, 'jumlah_bijih_CAD' => 20000000, "jumlah_logam_SD" => 1000000, 'jumlah_logam_CAD' => 20000000],
+        ['tahun_neraca' => "2016", "jumlah_bijih_SD" => 1000000, 'jumlah_bijih_CAD' => 20000000, "jumlah_logam_SD" => 1000000, 'jumlah_logam_CAD' => 20000000],
+        ['tahun_neraca' => "2017", "jumlah_bijih_SD" => 1000000, 'jumlah_bijih_CAD' => 20000000, "jumlah_logam_SD" => 1000000, 'jumlah_logam_CAD' => 20000000],
+        ['tahun_neraca' => "2017", "jumlah_bijih_SD" => 1000000, 'jumlah_bijih_CAD' => 20000000, "jumlah_logam_SD" => 1000000, 'jumlah_logam_CAD' => 20000000],
+        ['tahun_neraca' => "2018", "jumlah_bijih_SD" => 1000000, 'jumlah_bijih_CAD' => 20000000, "jumlah_logam_SD" => 1000000, 'jumlah_logam_CAD' => 20000000],
+        ['tahun_neraca' => "2018", "jumlah_bijih_SD" => 7000000, 'jumlah_bijih_CAD' => 20000000, "jumlah_logam_SD" => 1000000, 'jumlah_logam_CAD' => 20000000],
+        ['tahun_neraca' => "2018", "jumlah_bijih_SD" => 1000000, 'jumlah_bijih_CAD' => 20000000, "jumlah_logam_SD" => 1000000, 'jumlah_logam_CAD' => 20000000],
+        ['tahun_neraca' => "2015", "jumlah_bijih_SD" => 1000000, 'jumlah_bijih_CAD' => 20000000, "jumlah_logam_SD" => 1000000, 'jumlah_logam_CAD' => 20000000],
+        ['tahun_neraca' => "2015", "jumlah_bijih_SD" => 1000000, 'jumlah_bijih_CAD' => 20000000, "jumlah_logam_SD" => 1000000, 'jumlah_logam_CAD' => 20000000],
+        ['tahun_neraca' => "2016", "jumlah_bijih_SD" => 1000000, 'jumlah_bijih_CAD' => 20000000, "jumlah_logam_SD" => 1000000, 'jumlah_logam_CAD' => 20000000],
+        ['tahun_neraca' => "2016", "jumlah_bijih_SD" => 2000000, 'jumlah_bijih_CAD' => 20000000, "jumlah_logam_SD" => 1000000, 'jumlah_logam_CAD' => 20000000],
+        ['tahun_neraca' => "2016", "jumlah_bijih_SD" => 1000000, 'jumlah_bijih_CAD' => 20000000, "jumlah_logam_SD" => 1000000, 'jumlah_logam_CAD' => 20000000],
+        ['tahun_neraca' => "2017", "jumlah_bijih_SD" => 1000000, 'jumlah_bijih_CAD' => 20000000, "jumlah_logam_SD" => 1000000, 'jumlah_logam_CAD' => 20000000],
+        ['tahun_neraca' => "2017", "jumlah_bijih_SD" => 1000000, 'jumlah_bijih_CAD' => 20000000, "jumlah_logam_SD" => 1000000, 'jumlah_logam_CAD' => 20000000],
+        ['tahun_neraca' => "2018", "jumlah_bijih_SD" => 1000000, 'jumlah_bijih_CAD' => 20000000, "jumlah_logam_SD" => 1000000, 'jumlah_logam_CAD' => 20000000],
+        ['tahun_neraca' => "2018", "jumlah_bijih_SD" => 7000000, 'jumlah_bijih_CAD' => 20000000, "jumlah_logam_SD" => 1000000, 'jumlah_logam_CAD' => 20000000],
+        ['tahun_neraca' => "2018", "jumlah_bijih_SD" => 1000000, 'jumlah_bijih_CAD' => 20000000, "jumlah_logam_SD" => 1000000, 'jumlah_logam_CAD' => 20000000],
+    ];
 
-$tmp = [];
+    $data2 = [
+        ["tahun_neraca" => "2015", "jumlah_bijih_SD" => "32250000", "jumlah_bijih_CAD" => "0", "jumlah_logam_SD" => "3.87", "jumlah_logam_CAD" => "0"],
+        ["tahun_neraca" => "2016", "jumlah_bijih_SD" => "32250000", "jumlah_bijih_CAD" => "0", "jumlah_logam_SD" => "3.87", "jumlah_logam_CAD" => "0"],
+        ["tahun_neraca" => "2017", "jumlah_bijih_SD" => "32250000", "jumlah_bijih_CAD" => "0", "jumlah_logam_SD" => "3.87", "jumlah_logam_CAD" => "0"],
+        ["tahun_neraca" => "2018", "jumlah_bijih_SD" => "32250000", "jumlah_bijih_CAD" => "0", "jumlah_logam_SD" => "3.87", "jumlah_logam_CAD" => "0"],
+        ["tahun_neraca" => "2019", "jumlah_bijih_SD" => 32250000, "jumlah_bijih_CAD" => 0, "jumlah_logam_SD" => 3.87, "jumlah_logam_CAD" => 0]
+    ];
 
-$tmpTahun = [
-    'tahun'=> 0, 
-    'total_bijih_sd' => 0,
-    'total_bijih_cd' => 0,
-    'total_logam_sd' => 0,
-    'total_logam_cd' => 0,
-];
-
-$arrayTahun = [];
-
-$idxTahun = 0;
-$i = 0;
-foreach($dataRaw as $element) {
-    if(!in_array($element['tahun_neraca'], $arrayTahun)){
-        $arrayTahun[$idxTahun] = $element['tahun_neraca'];
-        $idxTahun++;
-    }
    
-    $i++;
-}
-
-// print_r($arrayTahun[0]);
-// echo json_encode($arrayTahun);
-print(count($arrayTahun));
 
 
-$dataOk = [];
-$i = 0;
-foreach($dataRaw as $d){
-    for ($i=0; $i < count($arrayTahun); $i++) { 
-        # code...
-        if($d['tahun_neraca'] == '2015'){
-            $tmpTahun['tahun']= $d['tahun_neraca'];
-            $tmpTahun['total_bijih_sd'] += $d['jumlah_bijih_SD'];
-            $tmpTahun['total_bijih_cd'] += $d['jumlah_bijih_CAD'];
-            $tmpTahun['total_logam_sd'] += $d['jumlah_logam_SD'];
-            $tmpTahun['total_logam_cd'] += $d['jumlah_logam_CAD'];
-            $i++;
-        }
-    }
     
-}
-array_push($dataOk, $tmpTahun);
+    function unpackArray($dataRaw) {
 
-// array_push($dataOk, $tmpTahun2);
-// $dataOk= $tmpTahun;
+        
+        $dataArray = []; //Arrray for result
+        $arrayKey = []; //Array for collect unique key on parameter
 
-echo json_encode($dataOk);
+        foreach($dataRaw as $data) {
+            if(!in_array($data['tahun_neraca'], $arrayKey)){
+                $arrayKey[] = $data['tahun_neraca'];
+            }
+           
+        }
 
-// $a = array(
-//     '1' => '3',
-//     '1' => '2',
-//     '2' => '1',
-//     '1' => '2',
-//     '2' => '2',
-//     );
+        $length = count($arrayKey);
 
-// $b = array ();
-// foreach ($a as $key => $value)
-// {
-//     if (isset($b[$key]))
-//     {
-//         $b[$key] += $value;
-//     }
-//     else
-//     {
-//         $b[$key] = $value;
-//     }
-// }
-// print_r($b);
+        foreach ($dataRaw as $data) {
 
-$array = [
-    ['Shop' => 3, 'Weight' => 4],
-    ['Shop' => 3, 'Weight' => 5],
-    ['Shop' => 3, 'Weight' => 1]
-];
-// echo json_encode($array);
-// print_r($array);
+            for ($i=0; $i < $length; $i++) {
+               
+                if($data['tahun_neraca'] == $arrayKey[$i]){
+    
+                    if(empty($dataArray)) {
+                        // echo "masuk data awal \n"; //for check proses add new value if the first array value
+                        $dataArray[$data['tahun_neraca']]['jumlah_bijih_SD'] = (float)$data['jumlah_bijih_SD'];
+                        $dataArray[$data['tahun_neraca']]['jumlah_bijih_CAD'] = (float)$data['jumlah_bijih_CAD'];
+                        $dataArray[$data['tahun_neraca']]['jumlah_logam_SD'] = (float)$data['jumlah_logam_SD'];
+                        $dataArray[$data['tahun_neraca']]['jumlah_logam_CAD'] = (float)$data['jumlah_logam_CAD'];
+                    }else{
+                        if(!isset($dataArray[$data['tahun_neraca']])) {
+                            // echo "tahun baru \n"; //for check proses add new value for next array value
+                            $dataArray[$data['tahun_neraca']]['jumlah_bijih_SD'] = isset($data['jumlah_bijih_CAD']) ? $data['jumlah_bijih_SD'] : null;
+                            $dataArray[$data['tahun_neraca']]['jumlah_bijih_CAD'] = isset($data['jumlah_bijih_CAD']) ? $data['jumlah_bijih_CAD'] : null;
+                            $dataArray[$data['tahun_neraca']]['jumlah_logam_SD'] = isset($data['jumlah_logam_SD']) ? $data['jumlah_logam_SD'] : null;
+                            $dataArray[$data['tahun_neraca']]['jumlah_logam_CAD'] = isset($data['jumlah_logam_CAD']) ? $data['jumlah_logam_CAD'] : null;
+                            
+                        }else{
+                            // echo "jumlah nilai \n"; //for check proses increment array value
+                            $dataArray[$data['tahun_neraca']]['jumlah_bijih_SD'] += (float)$data['jumlah_bijih_SD'];
+                            $dataArray[$data['tahun_neraca']]['jumlah_bijih_CAD'] += (float)$data['jumlah_bijih_CAD'];
+                            $dataArray[$data['tahun_neraca']]['jumlah_logam_SD'] += (float)$data['jumlah_logam_SD'];
+                            $dataArray[$data['tahun_neraca']]['jumlah_logam_CAD'] += (float)$data['jumlah_logam_CAD'];
+                        }
+                    }
+                }
+            }
+        }
 
-echo '<br>';
-$result = [];
-foreach($array as $item)
-{
-    if (isset($result[$item['Shop']]))
-    {
-        $result[$item['Shop']] += $item['Weight'];
+        // print_r($arrayKey); //for check result arrayKey
+        // print_r($dataArray); //for check result dataArray
+
+        return $dataArray; //array result
     }
-    else
-    {
-        $result[$item['Shop']] = $item['Weight'];
-    }
-}
-// echo json_encode($result);
 
+    //Output Type
+    header('Content-Type => application/json');
+    echo json_encode(unpackArray($data));
 
 ?>
